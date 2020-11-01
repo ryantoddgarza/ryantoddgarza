b0b09272c3059de3c88962935f7f46d9985f7185
ryantoddgarza <ryantoddgarza@gmail.com>
2020-10-31 21:57:37 -0700

    styling portfolio component

--- src/components/Portfolio/styled.js
+++ src/components/Portfolio/styled.js
@@ -3,10 +3,11 @@ import SimpleWrapper from '~/components/Common/SimpleWrapper';
 import { PRIMARY_COLOR } from '~/components/Common/constants';
 
 export const Wrapper = styled(SimpleWrapper)`
+  width: 90%;
+  margin: 0 auto;
   padding: 100px 0 0;
-
   @media (max-width: 414px) {
-    padding: 70px 16px 0;
+    padding: 70px 0 0;
   }
 
   &:before,
@@ -15,35 +16,53 @@ export const Wrapper = styled(SimpleWrapper)`
     content: '';
     clear: both;
   }
+
+  & > section {
+    float: left;
+    width: 50%;
+    max-height: calc(100vh - 100px);
+    overflow-y: scroll;
+    @media (max-width: 414px) {
+      float: none;
+      width: 100%;
+      max-height: unset;
+      overflow: visible;
+    }
+
+    ::-webkit-scrollbar {
+      width: 4px;
+      height: 6px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: ${PRIMARY_COLOR};
+      border-radius: 6px;
+    }
+  }
 `;
 
 export const PortfolioDescription = styled.section`
-  float: left;
-  padding: 0 0 0 5%;
-  width: 50%;
+  padding: 0 16px 16px 0;
   font-size: 14px;
   line-height: 1.6em;
-
   @media (max-width: 414px) {
-    float: none;
     margin: 0 0 16px;
     padding: 0 0 16px;
-    width: 100%;
   }
 
   h1 {
-    margin: 24px 0 10px;
-    font-size: 24px;
+    margin: 16px 0 10px;
+    font-size: 32px;
   }
 
   h2 {
     margin: 24px 0 10px;
-    font-size: 21px;
+    font-size: 28px;
   }
 
   h3 {
     margin: 24px 0 10px;
-    font-size: 19px;
+    font-size: 24px;
   }
 
   p {
@@ -81,25 +100,10 @@ export const PortfolioDescription = styled.section`
 `;
 
 export const PortfolioImages = styled.section`
-  float: left;
-  padding: 0 5% 0 0;
-  width: 50%;
-  max-height: calc(100vh - 100px);
-  overflow-y: scroll;
-
-  @media (max-width: 414px) {
-    float: none;
-    padding: 0;
-    width: 100%;
-    height: auto;
-    overflow-y: visible;
-  }
-
   img {
     padding: 16px;
     width: 100%;
     height: auto;
-
     @media (max-width: 414px) {
       float: left;
       margin: 0 0 8px;
@@ -108,15 +112,5 @@ export const PortfolioImages = styled.section`
         margin: 0 0 16px;
       }
     }
-  }
-
-  ::-webkit-scrollbar {
-    width: 4px;
-    height: 6px;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background-color: ${PRIMARY_COLOR};
-    border-radius: 6px;
   }
 `;

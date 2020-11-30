5a1bf71ce367cb7638a8e8568614f395acb16fe4
ryantoddgarza <ryantoddgarza@gmail.com>
2020-11-30 14:39:29 -0600

    Adjust header spacing

--- src/components/Common/SectionHeader/styled.js
+++ src/components/Common/SectionHeader/styled.js
@@ -4,24 +4,24 @@ import { fontSizeFluid } from '~/components/Common/fontSize';
 import { DURATION_NORMAL, TIMING_BEZIER } from '~/components/Common/constants';
 
 export const Header = styled.header`
-  padding: 48px 0;
+  padding: ${space.x12} 0;
   text-align: center;
 
   a {
     font-size: 1rem;
     transition: all ${DURATION_NORMAL} ${TIMING_BEZIER};
 
-    &::after {
-      content: ' →';
-    }
-
     &:hover {
       color: ${primaryColor.default};
+    }
+
+    &::after {
+      content: ' →';
     }
   }
 `;
 
 export const Title = styled.h2`
-  margin: ${space.x6} 0;
+  margin: ${space.x6} 0 ${space.x4};
   font-size: ${fontSizeFluid(28, 2)};
 `;

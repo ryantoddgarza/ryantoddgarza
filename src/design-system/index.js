e031c936b5eb222d87e9d1dcbe5df90ebc80386b
ryantoddgarza <ryantoddgarza@gmail.com>
2020-12-01 18:09:25 -0600

    Add breakpoints

--- src/design-system/index.js
+++ src/design-system/index.js
@@ -1,3 +1,4 @@
+export { breakpoint } from './layout';
 export { primaryColor, backgroundColor, textColor } from './data/color.yaml';
 export { heading } from './data/font.yaml';
 export { space } from './data/space.yaml';

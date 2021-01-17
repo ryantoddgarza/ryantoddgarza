4b1410236955d28959d151abdd6a8710203f444e
ryantoddgarza <ryantoddgarza@gmail.com>
2021-01-17 15:46:14 -0600

    Return `null` from ScopedImage if src is falsy

--- src/components/Common/ScopedImage/index.jsx
+++ src/components/Common/ScopedImage/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ScopedImage = ({ src, alt }) => (
-  <img
-    src={src.includes('//') ? src : require(`~/resources/${src}`)}
-    alt={alt}
-  />
-);
+const ScopedImage = ({ src, alt }) => {
+  if (!src) {
+    return null;
+  }
+
+  return (
+    <img
+      src={src.includes('//') ? src : require(`~/resources/${src}`)}
+      alt={alt}
+    />
+  );
+};
 
 ScopedImage.propTypes = {
   src: PropTypes.string.isRequired,

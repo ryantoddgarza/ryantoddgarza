bfd2eb20d4464a7eae5e7749e51b2c68df0e8a7a
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-13 17:49:08 -0600

    feat: add site metadata

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathPrefix: '/',
+  url: 'https://ryantoddgarza.com',
+  title: 'Ryan Todd Garza',
+  description: 'A creative based in Los Angeles, CA',
+  image: '/site.png', // relative to site root
+  author: 'Ryan Todd Garza',
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,18 @@
+const site = require('./config');
+
+const pathPrefix = site.pathPrefix === '/' ? '' : site.pathPrefix;
+
 module.exports = {
+  pathPrefix,
+  siteMetadata: {
+    siteUrl: site.url,
+    siteName: site.title,
+    titleTemplate: `%s – ${site.title}`,
+    description: site.description,
+    image: site.image,
+    author: site.author,
+    email: site.email,
+    twitter: site.twitter,
+  },
   plugins: ['gatsby-plugin-styled-components'],
 };

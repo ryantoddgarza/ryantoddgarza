ea1b12103c172faaae5d5d9075967b8d293ce63b
ryantoddgarza <ryantoddgarza@gmail.com>
2021-06-19 16:26:40 -0500

    build: migrate to @bitpas/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,6 @@
-const off = 0;
-const warn = 1;
-const error = 2;
-
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-    browser: true,
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-  },
-  extends: ['airbnb'],
-  plugins: ['react', 'import'],
+  extends: ['@bitpas/eslint-config'],
+  plugins: ['import'],
   settings: {
     'import/resolver': {
       alias: {
@@ -25,67 +9,11 @@ module.exports = {
       },
     },
   },
-  rules: {
-    'comma-dangle': [
-      error,
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'only-multiline',
-      },
-    ],
-    'function-paren-newline': [error, 'consistent'],
-    'global-require': off,
-    'implicit-arrow-linebreak': off,
-    'import/no-deprecated': warn,
-    'import/no-dynamic-require': off,
-    'import/no-webpack-loader-syntax': off,
-    'import/prefer-default-export': off,
-    'indent': [
-      error,
-      2,
-      {
-        SwitchCase: 1,
-      },
-    ],
-    'jsx-a11y/anchor-is-valid': off,
-    'jsx-a11y/click-events-have-key-events': error,
-    'jsx-a11y/heading-has-content': off,
-    'jsx-a11y/href-no-hash': off,
-    'jsx-a11y/label-has-for': off,
-    'jsx-a11y/label-has-associated-control': off,
-    'jsx-a11y/mouse-events-have-key-events': off,
-    'jsx-a11y/no-autofocus': off,
-    'max-len': [
-      error,
-      150,
-      {
-        ignoreComments: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-      },
-    ],
-    'no-console': warn,
-    'no-lonely-if': off,
-    'no-multiple-empty-lines': [error, { max: error, maxEOF: error }],
-    'no-implicit-coercion': error,
-    'no-shadow': off,
-    'no-underscore-dangle': off,
-    'no-unused-vars': [
-      error,
-      { args: 'after-used', ignoreRestSiblings: false },
-    ],
-    'object-curly-newline': [error, { consistent: true }],
-    'prefer-spread': off,
-    'quote-props': [error, 'consistent'],
-  },
   overrides: [
     {
       files: ['gatsby-node.js'],
       rules: {
-        'import/no-extraneous-dependencies': off,
+        'import/no-extraneous-dependencies': 'off',
       },
     },
     {
@@ -94,7 +22,7 @@ module.exports = {
         'src/components/Post/index.jsx',
       ],
       rules: {
-        'react/no-danger': off,
+        'react/no-danger': 'off',
       },
     },
   ],

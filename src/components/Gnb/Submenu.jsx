ec3b7b5193b10a68a6d12b6c405b595f7a3502b9
ryantoddgarza <ryantoddgarza@gmail.com>
2021-08-30 10:43:24 -0500

    refactor: global header submenu

---
+++ src/components/Gnb/Submenu.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+const Submenu = ({ listItems, expanded }) => {
+  const getSubmenuClasses = () => {
+    const submenuClasses = ['submenu', expanded && 'expanded'];
+    return submenuClasses.join(' ');
+  };
+
+  return (
+    <div className={getSubmenuClasses()}>
+      {listItems.map(({ key }) => {
+        if (key === '__ALL__') {
+          return null;
+        }
+
+        return (
+          <div className="submenu-item" key={key}>
+            <Link className="nav-link" to={`/categories/${key}/1`}>
+              {key}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+Submenu.propTypes = {
+  expanded: PropTypes.bool.isRequired,
+  listItems: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default Submenu;

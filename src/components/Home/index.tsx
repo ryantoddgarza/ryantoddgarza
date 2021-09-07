405a39b390b83cbc2bbf4aba7cc3e5e788d403e5
ryantoddgarza <ryantoddgarza@gmail.com>
2021-09-06 21:09:02 -0500

    refactor: convert and render content as structured data query

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,37 +1,36 @@
 import React, { FunctionComponent } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { Hero } from '../../design/components';
+import { graphql, useStaticQuery } from 'gatsby';
+import { FeatureGrid, Hero } from '../../design/components';
 import SEO from '../../components/Common/SEO';
 
-const content = {
-  hero: {
-    heading: 'I build practical systems to actualize creative concepts.',
-    copy: "I'm Ryan Todd Garza. I draw from a broad background of interdisciplinary pattern abstraction to develop effective technical and creative solutions for artistic and entrepreneurial ventures.",
-  },
-};
-
 const Home: FunctionComponent = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      home: markdownRemark(fields: { path: { eq: "/home" } }) {
-        html
+  const {
+    content: {
+      childResourcesJson: { hero, features },
+    },
+  } = useStaticQuery(graphql`
+    query HomeQuery {
+      content: file(name: { eq: "content-home" }) {
+        childResourcesJson {
+          hero {
+            copy
+            heading
+          }
+          features {
+            body
+            heading
+          }
+        }
       }
     }
   `);
 
-  const { home } = data;
-
   return (
     <div className="home">
       <SEO />
-      <Hero heading={content.hero.heading} copy={content.hero.copy} />
+      <Hero heading={hero.heading} copy={hero.copy} />
       <div className="container section">
-        <div className="col size7of12-tablet size1of2-desktop size5of12-widescreen">
-          <div
-            className="markdown"
-            dangerouslySetInnerHTML={{ __html: home.html }}
-          />
-        </div>
+        <FeatureGrid features={features} />
       </div>
     </div>
   );

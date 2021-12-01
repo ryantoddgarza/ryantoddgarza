08ca9c673fc22426cc901db338fd0f5bda9eac0d
ryantoddgarza <ryantoddgarza@gmail.com>
2021-12-01 09:49:02 -0600

    feat: add cta buttons to hero

--- src/design/components/Hero/Hero.tsx
+++ src/design/components/Hero/Hero.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import type { FunctionComponent } from 'react';
+import { Link } from 'gatsby';
 import type { HeroProps } from './types';
 
-const Hero: FunctionComponent<HeroProps> = ({ heading, copy }: HeroProps) => {
+const Hero: FunctionComponent<HeroProps> = ({
+  heading,
+  copy,
+  btn1,
+  btn2,
+}: HeroProps) => {
   const getHeroClasses = () => {
     const heroClasses = ['hero'];
     return heroClasses.join(' ');
   };
 
+  const hasButton = btn1 || btn2;
+
   return (
     <div className={getHeroClasses()}>
       <div className="content">
-        <div className="body">
-          <div className="heading">{heading}</div>
-          <p className="copy">{copy}</p>
-        </div>
+        <div className="heading">{heading}</div>
+        <p className="copy">{copy}</p>
+        {hasButton && (
+          <div className="cta-row button-group">
+            {btn1 && (
+              <Link className="button dark wide" to={btn1.url}>
+                {btn1.name}
+              </Link>
+            )}
+            {btn2 && (
+              <Link className="button light wide" to={btn2.url}>
+                {btn2.name}
+              </Link>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/design/components/Hero/types.ts
+++ src/design/components/Hero/types.ts
@@ -1,4 +1,11 @@
+interface HeroButton {
+  name: string;
+  url: string;
+}
+
 export interface HeroProps {
   heading: string;
   copy: string;
+  btn1?: HeroButton;
+  btn2?: HeroButton;
 }

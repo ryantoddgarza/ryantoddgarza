6a6ca8370823025f94522432df0e13d2363aa2a7
ryantoddgarza <ryantoddgarza@gmail.com>
2022-01-04 19:41:35 -0600

    refactor: replace section header with headliner

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { FunctionComponent } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { Headliner } from '../../design/components';
 import SEO from '../SEO';
 
 const Contact: FunctionComponent = () => {
@@ -25,11 +26,13 @@ const Contact: FunctionComponent = () => {
   return (
     <div className="contact">
       <SEO title={title} />
+      <section className="layout--margin">
+        <div className="container">
+          <Headliner title={title} />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
-          <div className="header">
-            <div className="title">{title}</div>
-          </div>
           <div>
             <a className="button dark wide" href={emailBtn.url}>
               {emailBtn.name}

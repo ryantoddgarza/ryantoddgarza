775f499d5010900404ea76369bd5dcec9609d934
ryantoddgarza <ryantoddgarza@gmail.com>
2022-03-19 18:35:54 -0500

    refactor: render link data from resources/content

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import type { FunctionComponent } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import SEO from '../SEO';
-import { TitledSection, PostCard, cardAspect } from '../../design/components';
+import {
+  List,
+  listSize,
+  TitledSection,
+  PostCard,
+  cardAspect,
+} from '../../design/components';
 import { ALBUMS_PATH } from '../../constants';
 import { AlbumData } from '../Album';
 
 const Music: FunctionComponent = () => {
   const {
     featAlbums: { nodes: albums },
-    content: { title },
+    content: {
+      title,
+      links: { listen, credits },
+    },
   } = useStaticQuery(graphql`
     query MusicQuery {
       content: resourcesJson(name: { eq: "music" }) {
         title
+        links {
+          listen {
+            name
+            url
+          }
+          credits {
+            name
+            url
+          }
+        }
       }
       featAlbums: allProjectsJson(
         filter: {
@@ -42,6 +61,20 @@ const Music: FunctionComponent = () => {
     }
   `);
 
+  const parseLinkData = (arr: any[]) =>
+    arr.map(({ name, url }) => (
+      <div key={name}>
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          title={`Ryan Todd Garza on ${name}`}
+        >
+          {name}
+        </a>
+      </div>
+    ));
+
   return (
     <div className="music">
       <SEO title={title} />
@@ -68,6 +101,20 @@ const Music: FunctionComponent = () => {
                 )
               )}
             </div>
+          </TitledSection>
+        </section>
+        <section className="layout--margin">
+          <TitledSection title="Listen">
+            <List nodes={parseLinkData(listen)} size={listSize.LARGE} ordered />
+          </TitledSection>
+        </section>
+        <section className="layout--margin">
+          <TitledSection title="Credits">
+            <List
+              nodes={parseLinkData(credits)}
+              size={listSize.LARGE}
+              ordered
+            />
           </TitledSection>
         </section>
       </div>

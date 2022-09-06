ee56e8991fd7981b4b530f15e815ea150d654697
ryantoddgarza <ryantoddgarza@gmail.com>
2022-09-06 14:47:54 -0500

    refactor: remove deprecated TitledSection component and styles

--- src/design/components/TitledSection/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TitledSection } from './TitledSection';
-export * from './types';
--- src/design/components/index.ts
+++ src/design/components/index.ts
@@ -5,4 +5,3 @@ export * from './GlobalHeader';
 export * from './Hero';
 export * from './List';
 export * from './PostCard';
-export * from './TitledSection';

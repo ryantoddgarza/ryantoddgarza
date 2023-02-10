be07c7025beb78b7afe42f3df7e2d72ad403bdb9
ryantoddgarza <ryantoddgarza@gmail.com>
2023-02-10 10:29:35 -0600

    refactor: remove dance curriculum vitae

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -3,7 +3,6 @@ exports.ALBUMS_PATH = '/albums';
 exports.CATEGORIES_PATH = '/categories';
 exports.CODE_PATH = '/code';
 exports.CONTACT_PATH = '/contact';
-exports.DANCE_CV_PATH = '/dance-cv';
 exports.HOME_PATH = '/';
 exports.MUSIC_PATH = '/music';
 exports.POSTS_PATH = '/posts';

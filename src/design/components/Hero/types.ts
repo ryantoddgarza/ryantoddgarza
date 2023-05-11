d675b8a474b4d17c9d25f6ac4cd61d7fb645f07f
ryantoddgarza <ryantoddgarza@gmail.com>
2023-05-11 12:14:39 -0700

    refactor: remove deprecated hero component

--- src/design/components/Hero/types.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react';
-
-export interface HeroProps {
-  heading: string;
-  body: string;
-  children?: ReactNode;
-}
--- src/design/components/index.ts
+++ src/design/components/index.ts
@@ -1,5 +1,4 @@
 export * from './ErrorPage';
 export * from './GlobalFooter';
 export * from './GlobalHeader';
-export * from './Hero';
 export * from './List';

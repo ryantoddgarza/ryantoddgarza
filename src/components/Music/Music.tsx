6711745696e6ef3d712769cb7512ee6d1788bd86
ryantoddgarza <ryantoddgarza@gmail.com>
2023-09-29 10:55:33 -0500

    Refactor music component styles

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -3,7 +3,7 @@ import type { FunctionComponent } from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import SEO from '../SEO';
 import { PostCard, cardAspect } from '../PostCard';
-import List, { listSize } from '../List';
+import List, { listSize, listTheme } from '../List';
 import { ALBUMS_PATH } from '../../constants';
 import type { MusicRelease } from '../../../lib/contentful/generated';
 
@@ -68,33 +68,37 @@ const Music: FunctionComponent = () => {
       <SEO title={title} />
       <section className="section light">
         <div className="container">
-          <div className="module">
-            <h2 className="u-h2">Featured Albums</h2>
-            <div className="u-h6 link">
-              <Link to={ALBUMS_PATH}>View all albums</Link>
+          <div className="explore-module module">
+            <div className="header">
+              <h2 className="title">Featured Albums</h2>
+              <div className="cta">
+                <Link to={ALBUMS_PATH}>View all albums</Link>
+              </div>
             </div>
-          </div>
-          <div className="featured-albums">
-            <div className="post-grid">
-              {albums.map(
-                ({
-                  id,
-                  slug,
-                  title,
-                  releaseDate,
-                  artist,
-                  image,
-                }: MusicRelease) => (
-                  <PostCard
-                    key={id}
-                    title={title}
-                    subtitle={`${artist?.name} — ${releaseDate?.split('-')[0]}`}
-                    image={image?.gatsbyImage}
-                    path={`/${slug}`}
-                    aspect={cardAspect.SQUARE}
-                  />
-                )
-              )}
+            <div className="body">
+              <div className="post-grid">
+                {albums.map(
+                  ({
+                    id,
+                    slug,
+                    title,
+                    releaseDate,
+                    artist,
+                    image,
+                  }: MusicRelease) => (
+                    <PostCard
+                      key={id}
+                      title={title}
+                      subtitle={`${artist?.name} — ${
+                        releaseDate?.split('-')[0]
+                      }`}
+                      image={image?.gatsbyImage}
+                      path={`/${slug}`}
+                      aspect={cardAspect.SQUARE}
+                    />
+                  )
+                )}
+              </div>
             </div>
           </div>
         </div>
@@ -102,27 +106,39 @@ const Music: FunctionComponent = () => {
       <section className="section light">
         <div className="container">
           <div className="explore-grid">
-            <div className="module">
-              <h2 className="u-h2">Listen</h2>
-              <List
-                nodes={parseLinkData(listen)}
-                size={listSize.LARGE}
-                ordered
-              />
+            <div className="explore-module module">
+              <div className="header">
+                <h2 className="title">Listen</h2>
+              </div>
+              <div className="body">
+                <List
+                  nodes={parseLinkData(listen)}
+                  size={listSize.LARGE}
+                  theme={listTheme.DARK}
+                  ordered
+                />
+              </div>
             </div>
-            <div className="module">
-              <h2 className="u-h2">Credits</h2>
-              <List
-                nodes={parseLinkData(credits)}
-                size={listSize.LARGE}
-                ordered
-              />
+            <div className="explore-module module">
+              <div className="header">
+                <h2 className="title">Credits</h2>
+              </div>
+              <div className="body">
+                <List
+                  nodes={parseLinkData(credits)}
+                  size={listSize.LARGE}
+                  theme={listTheme.DARK}
+                  ordered
+                />
+              </div>
             </div>
-            <div className="module inquire">
-              <h2 className="u-h3">Inquire</h2>
-              <p className="u-copy margin">
-                Email for licensing, press, and radio.
-              </p>
+            <div className="explore-module module inquire">
+              <div className="header">
+                <h2 className="title">Inquire</h2>
+              </div>
+              <div className="body">
+                <p className="u-copy">Email for licensing, press, and radio.</p>
+              </div>
             </div>
           </div>
         </div>

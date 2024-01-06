100618e95421aadef73a359a1549c9ac1daab241
ryantoddgarza <ryantoddgarza@gmail.com>
2024-01-06 12:29:16 -0600

    Hard code home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '14px',
+          fontWeight: '400'
+        }}
+      >Breathe.</h1>
+    </main>
+  );
 }

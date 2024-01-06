03d8c0c8e5e621cdc0c19ef1b6693eb1bea725f6
ryantoddgarza <ryantoddgarza@gmail.com>
2024-01-06 12:09:37 -0600

    Render layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,34 @@
-import type { Metadata } from 'next';
+import { createClient } from '@/prismicio';
+import { isFilled } from '@prismicio/client';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export async function generateMetadata() {
+  const client = createClient();
+  const {
+    data: { favicon, site_name, site_description },
+  } = await client.getSingle('settings');
+  const title = isFilled.keyText(site_name) ? site_name : '';
+  const description = isFilled.keyText(site_description) ? site_description : '';
+  const icon = isFilled.image(favicon) ? favicon.url : undefined;
+
+  const metadata: Metadata = {
+    metadataBase: new URL('https://ryantoddgarza.com'),
+    title: {
+      template: `%s | ${title}`,
+      default: title,
+    },
+    description: description,
+    icons: {
+      icon: icon,
+    },
+  };
+
+  return metadata;
+}
 
 export default function RootLayout({
   children,

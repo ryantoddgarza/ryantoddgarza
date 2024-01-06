283fd7f4b64bab230498bb5cd4fd5ccd38cf196d
ryantoddgarza <ryantoddgarza@gmail.com>
2024-01-06 12:20:22 -0600

    Add Tripoli CSS reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@/prismicio';
 import { isFilled } from '@prismicio/client';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './globals.scss';
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,6 +11,7 @@ export async function generateMetadata() {
   const {
     data: { favicon, site_name, site_description },
   } = await client.getSingle('settings');
+
   const title = isFilled.keyText(site_name) ? site_name : '';
   const description = isFilled.keyText(site_description) ? site_description : '';
   const icon = isFilled.image(favicon) ? favicon.url : undefined;
@@ -30,11 +31,7 @@ export async function generateMetadata() {
   return metadata;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>

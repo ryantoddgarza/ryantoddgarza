e3d61600c58d85140aac38a851b43335ef76a1a2
ryantoddgarza <ryantoddgarza@gmail.com>
2024-01-16 10:45:16 -0600

    Build and style TextBlockSection (temporarily fullpage)

--- src/slices/TextBlockSection/index.tsx
+++ src/slices/TextBlockSection/index.tsx
@@ -1,18 +1,28 @@
 import { Content } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
+import styles from './textBlockSection.module.scss';
 
-/**
- * Props for `TextBlockSection`.
- */
-export type TextBlockSectionProps = SliceComponentProps<Content.TextBlockSectionSlice>;
+export type TextBlockSectionProps =
+  SliceComponentProps<Content.TextBlockSectionSlice>;
 
-/**
- * Component for "TextBlockSection" Slices.
- */
 const TextBlockSection = ({ slice }: TextBlockSectionProps): JSX.Element => {
+  function getSectionClasses(): string {
+    const classes: string[] = [styles.base];
+
+    return classes.join(' ');
+  }
+
   return (
-    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      Placeholder component for text_block_section (variation: {slice.variation}) Slices
+    <section
+      className={getSectionClasses()}
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <PrismicRichText field={slice.primary.body_copy} />
+        </div>
+      </div>
     </section>
   );
 };
